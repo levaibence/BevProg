d102d5ff0445fa1e34425498ba1fb3549c958932
Levai Bence <levai.bence@shrek.unideb.hu>
2021-10-27 05:59:44 +0200

    05drill

---
+++ Ch05/05drill07.cpp
@@ -0,0 +1,18 @@
+#include "std_lib_facilities.h"
+int main()
+try {
+	  if (true) cout << "Success!\n";
+	  else cout << "Fail!\n";
+          keep_window_open();
+          return 0;
+}
+catch (exception& e) {
+          cerr << "error: " << e.what() << '\n';
+          keep_window_open();
+          return 1;
+}
+catch (...) {
+          cerr << "Oops: unknown exception!\n";
+          keep_window_open();
+          return 2;
+}
---
+++ Ch05/05drill23.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+int main()
+try {
+          int j=0,i=0;
+	  while (i<10) ++i;
+	  if (j<i) cout << "Success!\n";
+          keep_window_open();
+          return 0;
+}
+catch (exception& e) {
+          cerr << "error: " << e.what() << '\n';
+          keep_window_open();
+          return 1;
+}
+catch (...) {
+          cerr << "Oops: unknown exception!\n";
+          keep_window_open();
+          return 2;
+}
